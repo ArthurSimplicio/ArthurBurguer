9f334fcd5dfa2493f4420f72c462c3f40c42d8eb
ArthurSimplicio <arthurpark20@gmail.com>
2024-09-08 19:41:04 -0300

    new prices

--- src/services/itens.tsx
+++ src/services/itens.tsx
@@ -10,12 +10,12 @@ import Burguer8 from "../assets/hamburguer8.jpg"
 
 
 export const itens: Array<ItemType> = [
-    { name: 'Bom Burguer', image: Burguer, price: 15.90, description: 'Pão de Trigo quentinho com carne artesanal, maionese e molho especial' },
-    { name: 'Oásis no Deserto', image: Burguer2, price: 15.90, description: 'Com carne suculenta e molho barbecue, uma experiência saborosa e reconfortante.' },
+    { name: 'Bom Burguer', image: Burguer, price: 14.90, description: 'Pão de Trigo quentinho com carne artesanal, maionese e molho especial' },
+    { name: 'Oásis no Deserto', image: Burguer2, price: 16, description: 'Com carne suculenta e molho barbecue, uma experiência saborosa e reconfortante.' },
     { name: 'Eclipse Total', image: Burguer3, price: 15.90, description: 'carne temperada com cominho e coentro, queijo brie e cebola caramelizada, evocando eclipse' },
-    { name: 'Furacão de Bacon', image: Burguer4, price: 15.90, description: 'três fatias generosas de bacon crocante, queijo cheddar, maionese de bacon e cebola roxa grelhada.' },
-    { name: 'Mini Burguer', image: Burguer5, price: 15.90, description: 'Pão de Trigo quentinho com carne artesanal com maionese e molho especial, tamanho mini' },
-    { name: 'Conforto no Pãor', image: Burguer6, price: 15.90, description: 'Com base de lentilha, um toque de alecrim, para um sabor caseiro e reconfortante.' },
-    { name: 'Dragão Vermelho', image: Burguer7, price: 15.90, description: 'Para os aventureiros, com carne temperada com pimenta caiena e maionese de chipotle.' },
-    { name: 'Jardim Secreto', image: Burguer8, price: 15.90, description: 'Um vegetariano feito com blend de cogumelos salteados, queijo vegano e rúcula fresca, tudo em um pão macio.' },
+    { name: 'Furacão de Bacon', image: Burguer4, price: 18.90, description: 'três fatias generosas de bacon crocante, queijo cheddar, maionese de bacon e cebola roxa grelhada.' },
+    { name: 'Mini Burguer', image: Burguer5, price: 10, description: 'Pão de Trigo quentinho com carne artesanal com maionese e molho especial, tamanho mini' },
+    { name: 'Conforto no Pão', image: Burguer6, price: 17, description: 'Com base de lentilha, um toque de alecrim, para um sabor caseiro e reconfortante.' },
+    { name: 'Dragão Vermelho', image: Burguer7, price: 20.90, description: 'Para os aventureiros, com carne temperada com pimenta caiena e maionese de chipotle.' },
+    { name: 'Jardim Secreto', image: Burguer8, price: 22.90, description: 'Um vegetariano feito com blend de cogumelos salteados, queijo vegano e rúcula fresca, tudo em um pão macio.' },
 ]
